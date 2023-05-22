80913b5ac4e67ca9e62033b11db21b012be1f1ff
Maciej Gojlik <60773657+Pompeiro@users.noreply.github.com>
2023-05-22 10:17:22 +0200

    Add sample TextElement usage (#3)

--- src/pages/login.py
+++ src/pages/login.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page
 
 from src.pages.base import BasePage
+from src.pages.elements.elements import TextElement
 
 
 class LoginPage(BasePage):
@@ -10,6 +11,9 @@ class LoginPage(BasePage):
         self.username_field = self.page.get_by_test_id("username")
         self.password_field = self.page.get_by_test_id("password")
         self.login_button = self.page.get_by_test_id("login-button")
+        self.login_validation_error_message = TextElement(
+            locator=self.page.get_by_test_id("error"), static_text="Epic sadface: "
+        )
 
     def login(self, email: str, password: str) -> None:
         self.username_field.fill(email)
--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -4,7 +4,9 @@ from src.models.user import User
 from src.pages import InventoryPage, LoginPage
 
 
-def test_user_should_login_using_valid_credentials(page: Page, standard_user: User):
+def test_user_should_login_using_valid_credentials(
+    page: Page, standard_user: User
+) -> None:
     login_page = LoginPage(page)
     inventory_page = InventoryPage(page)
 
@@ -14,3 +16,17 @@ def test_user_should_login_using_valid_credentials(page: Page, standard_user: Us
 
     expect(page).to_have_url(inventory_page.combine_url())
     expect(inventory_page.sort_dropdown).to_be_visible()
+
+
+def test_login_validation_error_should_have_username_and_password_mismatch_message_using_invalid_credentials(
+    page: Page,
+) -> None:
+    login_page = LoginPage(page)
+
+    login_page.navigate()
+    page.wait_for_load_state("load")
+    login_page.login(email="asd", password="sda")
+
+    login_page.login_validation_error_message.verify(
+        dynamic_text="Username and password do not match any user in this service"
+    )
